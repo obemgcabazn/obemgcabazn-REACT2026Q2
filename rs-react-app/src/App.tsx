293c4fa89909bf66e7469c95cc539d9ad1bf9bf6
Alexandr Khokhriakov <obemgghost@ya.ru>
2026-05-16 11:50:09 +0300

    feat: migrate App to FC

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import StorageHelper from './controller/StorageHelper.ts';
 import SearchForm from './components/SearchForm.tsx';
 import PokemonsList from './components/PokemonsList.tsx';
@@ -8,67 +8,51 @@ import ErrorHandler from './components/ErrorHandler.tsx';
 import TestError from './components/TestError.tsx';
 import type { Pokemon } from 'pokeapi-typescript';
 
-interface AppState {
-  currentSearchQuery: string;
-  loading: boolean;
-  error: Error | null;
-  result: PokemonInList[] | Pokemon | null;
-}
-
 interface PokemonInList {
   name: string;
   url: string;
 }
 
-export default class App extends Component<Record<string, never>, AppState> {
-  state: AppState = {
-    currentSearchQuery: StorageHelper.get('searchQuery') ?? '',
-    loading: false,
-    error: null,
-    result: null,
-  };
+const App = () => {
+  const savedQuery = StorageHelper.get<string>('searchQuery') ?? '';
+  const [searchQuery, setSearchQuery] = useState(savedQuery);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [result, setResult] = useState<PokemonInList[] | Pokemon | null>(null);
 
-  componentDidMount() {
-    if (!this.state.currentSearchQuery) {
-      this.fetchAllPokemons();
+  useEffect(() => {
+    if (searchQuery) {
+      fetchPokemonByName(searchQuery.toString());
     } else {
-      this.fetchPokemonByName(this.state.currentSearchQuery);
+      fetchAllPokemons();
     }
-  }
+  }, [savedQuery]);
 
-  componentDidUpdate(_prevProps: Record<string, never>, prevState: AppState) {
-    if (prevState.currentSearchQuery !== this.state.currentSearchQuery) {
-      if (!this.state.currentSearchQuery) {
-        this.fetchAllPokemons();
-      } else {
-        this.fetchPokemonByName(this.state.currentSearchQuery);
-      }
-    }
-  }
+  const fetchAllPokemons = async () => {
+    setLoading(true);
+    setError(null);
 
-  fetchAllPokemons = async () => {
-    this.setState({ loading: true, error: null });
     try {
       const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/?limit=10`
       );
       const data: { results: PokemonInList[] } = await response.json();
       if (data) {
-        this.setState({ result: data.results });
+        setResult(data.results);
       }
     } catch (err) {
-      this.setState({
-        error:
-          err instanceof Error ? err : new Error('Error while loading data'),
-      });
+      setError(
+        err instanceof Error ? err : new Error('Error while loading data')
+      );
+      setResult(null);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  fetchPokemonByName = async (name: string) => {
-    this.setState({ loading: true });
-    this.setState({ error: null });
+  const fetchPokemonByName = async (name: string) => {
+    setLoading(true);
+    setError(null);
 
     try {
       const response = await fetch(
@@ -78,29 +62,20 @@ export default class App extends Component<Record<string, never>, AppState> {
         throw new Error(String(response.status));
       }
       const data = await response.json();
-      this.setState({ result: data });
+      setResult(data);
     } catch (err) {
-      this.setState({
-        error:
-          err instanceof Error ? err : new Error('Error while loading data'),
-        result: null,
-      });
+      setError(
+        err instanceof Error ? err : new Error('Error while loading data')
+      );
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
   };
 
-  setSearchQuery = (searchQuery: string) => {
-    this.setState({ currentSearchQuery: searchQuery });
-  };
-
-  bottomSection = () => {
-    const { loading, error, result, currentSearchQuery } = this.state;
-
+  const bottomSection = () => {
     if (loading) return <Spinner />;
     if (error) return <ErrorHandler errorData={error} />;
-
-    if (currentSearchQuery && result && !Array.isArray(result)) {
+    if (searchQuery && result && !Array.isArray(result)) {
       return <OnePokemon pokemon={result} />;
     }
 
@@ -108,20 +83,16 @@ export default class App extends Component<Record<string, never>, AppState> {
       return <PokemonsList pokemonsList={result} />;
     }
   };
-
-  render() {
-    return (
-      <div className="container">
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-          <h1>Pokemon Searching App</h1>
-          <TestError />
-        </div>
-        <SearchForm
-          searchQuery={this.state.currentSearchQuery}
-          onSearch={this.setSearchQuery}
-        />
-        {this.bottomSection()}
+  return (
+    <div className="container">
+      <div className="flex-aic-sb">
+        <h1>Pokemon Searching App</h1>
+        <TestError />
       </div>
-    );
-  }
-}
+      <SearchForm searchQuery={searchQuery} onSearch={setSearchQuery} />
+      {bottomSection()}
+    </div>
+  );
+};
+
+export default App;
