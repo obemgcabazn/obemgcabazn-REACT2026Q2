075bbd37af4b20866215b746cf04b225aad836a5
Alexandr Khokhriakov <obemgghost@ya.ru>
2026-05-16 08:41:21 +0300

    fix: migrate Spinner to FC

--- rs-react-app/src/components/Spinner.tsx
+++ rs-react-app/src/components/Spinner.tsx
@@ -1,24 +1,21 @@
 import './Spinner.scss';
-import { PureComponent } from 'react';
 import Pokebal from '../assets/pokeball.svg';
 
-export default class Spinner extends PureComponent {
-  render() {
-    return (
-      <div className="container">
-        <div className="spinner__wrapper">
-          <div>
-            <img
-              className="spinner"
-              src={Pokebal}
-              alt="Loading..."
-              width="80"
-              height="80"
-            />
-          </div>
-          <p>Loading...</p>
-        </div>
+const Spinner = () => {
+  return (
+    <div className={'container'}>
+      <div className={'spinner_wrapper'}>
+        <img
+          className="spinner"
+          src={Pokebal}
+          alt="Loading..."
+          width="80"
+          height="80"
+        />
+        <p>Loading...</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Spinner;
